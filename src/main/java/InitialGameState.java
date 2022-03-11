c8224cc1acf888cbb52b58fd803921f689a00926
Philippe Marschall <philippe.marschall@gmail.com>
2022-03-11 11:14:56 +0100

    make inner classes private

--- src/main/java/InitialGameState.java
+++ src/main/java/InitialGameState.java
@@ -4,23 +4,24 @@ import java.util.Objects;
 
 final class InitialGameState {
 
-    static final FlipableGameState ADVANTAGE_PLAYER1 = new AdvantagePlayer1();
+    private static final FlipableGameState ADVANTAGE_PLAYER1 = new AdvantagePlayer1();
 
-    static final FlipableGameState ADVANTAGE_PLAYER2 = new AdvantagePlayer2();
+    private static final FlipableGameState ADVANTAGE_PLAYER2 = new AdvantagePlayer2();
 
-    static final FlipableGameState WIN_PLAYER1 = new WinPlayer1();
+    private static final FlipableGameState WIN_PLAYER1 = new WinPlayer1();
 
-    static final FlipableGameState WIN_PLAYER2 = new WinPlayer2();
+    private static final FlipableGameState WIN_PLAYER2 = new WinPlayer2();
 
-    static final FlipableGameState DEUCE = new Deuce();
+    private static final FlipableGameState DEUCE = new Deuce();
 
-    static final GameState INITIAL_STATE = computeInitialState();
-    
-    interface FlipableGameState extends GameState {
+    private static final GameState INITIAL_STATE = computeInitialState();
+
+    private interface FlipableGameState extends GameState {
 
         FlipableGameState flip();
+
     }
-    
+
     private static GameState computeInitialState() {
 
         Map<ScoreLookupKey, FlipableGameState> computed = new HashMap<>();
@@ -31,14 +32,16 @@ final class InitialGameState {
         computed.put(new ScoreLookupKey(4, 3), ADVANTAGE_PLAYER1);
         computed.put(new ScoreLookupKey(3, 4), ADVANTAGE_PLAYER2);
 
-
+        // all wins that weren't proceeded with "Advantage player X"
         for (int i = 0; i <= 2; i++) {
             ScoreLookupKey key = new ScoreLookupKey(4, i);
             computed.put(key, WIN_PLAYER1);
             computed.put(key.flipPlayerScores(), WIN_PLAYER2);
         }
 
-        for (int i = 4; i > 0; i--) {
+        // No need to start with 4, because all 4 based states are already in the map.
+        // They are either wins or "Advantage player X".
+        for (int i = 3; i > 0; i--) {
             for (int j = i; j >= 0; j--) {
                 ScoreLookupKey key = new ScoreLookupKey(i, j);
                 if (!computed.containsKey(key)) {
@@ -117,7 +120,7 @@ final class InitialGameState {
 
     }
 
-    static String translate(int score) {
+    private static String translate(int score) {
         switch (score) {
         case 0:
             return "Love";
@@ -132,7 +135,7 @@ final class InitialGameState {
         }
     }
 
-    static final class GenericGameState implements FlipableGameState {
+    private static final class GenericGameState implements FlipableGameState {
 
         private final int scorePlayer1;
 
@@ -143,8 +146,11 @@ final class InitialGameState {
         private final FlipableGameState player2Wins;
 
         GenericGameState(int scorePlayer1, int scorePlayer2, FlipableGameState player1Wins, FlipableGameState player2Wins) {
-            if (scorePlayer1 >= 4 || scorePlayer2 >= 4) {
-                throw new IllegalArgumentException();
+            if (scorePlayer1 >= 4) {
+                throw new IllegalArgumentException(AdvantagePlayer1.class + " shold be used");
+            }
+            if (scorePlayer2 >= 4) {
+                throw new IllegalArgumentException(AdvantagePlayer2.class + " shold be used");
             }
             this.scorePlayer1 = scorePlayer1;
             this.scorePlayer2 = scorePlayer2;
@@ -188,7 +194,10 @@ final class InitialGameState {
 
     }
 
-    static final class All implements FlipableGameState {
+    /**
+     * Represents a state when both players have equal points but we haven't reached deuce yet.
+     */
+    private static final class All implements FlipableGameState {
 
         private final GameState player1Wins;
 
@@ -197,6 +206,12 @@ final class InitialGameState {
         private int score;
 
         All(int score, GameState player1Wins, GameState player2Wins) {
+            if (score > 2) {
+                throw new IllegalArgumentException("deuce should be used");
+            }
+            if (score < 0) {
+                throw new IllegalArgumentException("negative score not allowed");
+            }
             Objects.requireNonNull(player1Wins, "player1Wins");
             Objects.requireNonNull(player2Wins, "player2Wins");
             this.player1Wins = player1Wins;
@@ -237,7 +252,7 @@ final class InitialGameState {
 
     }
 
-    static final class Deuce implements FlipableGameState {
+    private static final class Deuce implements FlipableGameState {
 
         @Override
         public GameState player1WonPoint(GameState.GameDisplayContext context) {
@@ -272,7 +287,7 @@ final class InitialGameState {
 
     }
 
-    static abstract class Advantage implements FlipableGameState {
+    private static abstract class Advantage implements FlipableGameState {
 
         @Override
         public String getScore(GameState.GameDisplayContext context) {
@@ -283,7 +298,7 @@ final class InitialGameState {
 
     }
 
-    static final class AdvantagePlayer1 extends Advantage {
+    private static final class AdvantagePlayer1 extends Advantage {
 
         @Override
         public GameState player1WonPoint(GameState.GameDisplayContext context) {
@@ -314,7 +329,7 @@ final class InitialGameState {
 
     }
 
-    static final class AdvantagePlayer2 extends Advantage {
+    private static final class AdvantagePlayer2 extends Advantage {
 
         @Override
         public GameState player1WonPoint(GameState.GameDisplayContext context) {
@@ -345,7 +360,7 @@ final class InitialGameState {
 
     }
 
-    static abstract class WonGame implements FlipableGameState {
+    private static abstract class WonGame implements FlipableGameState {
 
         @Override
         public String getScore(GameState.GameDisplayContext context) {
@@ -370,7 +385,7 @@ final class InitialGameState {
 
     }
 
-    static final class WinPlayer1 extends WonGame {
+    private static final class WinPlayer1 extends WonGame {
 
         @Override
         protected String getPlayer(GameState.GameDisplayContext context) {
@@ -389,7 +404,7 @@ final class InitialGameState {
 
     }
 
-    static final class WinPlayer2 extends WonGame {
+    private static final class WinPlayer2 extends WonGame {
 
         @Override
         protected String getPlayer(GameState.GameDisplayContext context) {
